f0d6b51ef2725f0bafdc2d5e7f25c713316baf13
zsp <allen1989127@gmail.com>
2017-01-23 16:30:51 +0800

    fix channel define error:

--- src/org/freeman/led/led_controller.py
+++ src/org/freeman/led/led_controller.py
@@ -4,35 +4,36 @@
 import RPi.GPIO as GPIO
 import time
 
-class LedController:
+class _LedChannel() :
     RED_CHANNEL = 14
     GREEN_CHANNEL = 15
     BLUE_CHANNEL = 18
 
+class LedController:
     def __init__(self) :
         GPIO.cleanup()
         GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(RED_CHANNEL, GPIO.OUT)
-        GPIO.setup(GREEN_CHANNEL, GPIO.OUT)
-        GPIO.setup(BLUE_CHANNEL, GPIO.OUT)
+        GPIO.setup(_LedChannel.RED_CHANNEL, GPIO.OUT)
+        GPIO.setup(_LedChannel.GREEN_CHANNEL, GPIO.OUT)
+        GPIO.setup(_LedChannel.BLUE_CHANNEL, GPIO.OUT)
 
-        GPIO.output(RED_CHANNEL, GPIO.LOW)
-        GPIO.output(GREEN_CHANNEL, GPIO.LOW)
-        GPIO.output(BLUE_CHANNEL, GPIO.LOW)
+        GPIO.output(_LedChannel.RED_CHANNEL, GPIO.LOW)
+        GPIO.output(_LedChannel.GREEN_CHANNEL, GPIO.LOW)
+        GPIO.output(_LedChannel.BLUE_CHANNEL, GPIO.LOW)
 
         # start a loop with RGB in 3 seconds
-        self.rPWM = GPIO.PWM(RED_CHANNEL, 1)
+        self.rPWM = GPIO.PWM(_LedChannel.RED_CHANNEL, 1)
         self.rPWM.start(30)
         time.sleep(1)
         self.rPWM.stop()
 
-        self.gPWM = GPIO.PWM(GREEN_CHANNEL, 1)
+        self.gPWM = GPIO.PWM(_LedChannel.GREEN_CHANNEL, 1)
         self.gPWM.start(30)
         time.sleep(1)
         self.gPWM.stop()
 
-        self.bPWM = GPIO.PWM(BLUE_CHANNEL, 1)
+        self.bPWM = GPIO.PWM(_LedChannel.BLUE_CHANNEL, 1)
         self.bPWM.start(30)
         time.sleep(1)
         self.bPWM.stop()
